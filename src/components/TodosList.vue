765bcfed4432e77574f0ccc72fd88489c4de3a2d
Stanislav <svdunaev95@gmail.com>
2024-11-18 03:25:53 +0500

    fix issues, related with ts, remove some more unused code

---
+++ src/components/TodosList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>todos</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TodosList",
+});
+</script>
+
+<style></style>
